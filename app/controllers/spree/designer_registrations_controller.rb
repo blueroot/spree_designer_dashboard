20a51ab3b72702d0236ff2f83b5e738d69456bd3
Jamie Stephens <jamie@blueroot.com>
2014-02-26 06:01:33 -0600

    adding all the changes

--- app/controllers/spree/designer_registrations_controller.rb
+++ app/controllers/spree/designer_registrations_controller.rb
@@ -1,6 +1,7 @@
 class Spree::DesignerRegistrationsController < Spree::StoreController
   before_action :set_designer_registration, only: [:show, :edit, :update, :destroy]
-
+  layout "/spree/layouts/splash"
+  
   # GET /designer_registrations
   def index
     @designer_registrations = Spree::DesignerRegistration.all
