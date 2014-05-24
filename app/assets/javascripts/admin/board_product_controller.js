1fa7d365c0d38469e60f2fe581ea6f9d614a358c
Luke Parham <lappp9@gmail.com>
2014-05-24 12:57:11 -0500

    bailing on angular for the moment, but come back here if you remember why you wanted it so bad...

--- app/assets/javascripts/admin/board_product_controller.js
+++ app/assets/javascripts/admin/board_product_controller.js
@@ -1,45 +1,25 @@
 function board_product_controller($scope, $http, $location){
 
-  $scope.user_errors = ""
-  $scope.farmer_errors = ""
+  $scope.variants = {};
+  $scope.variants['12881'] = {};
+  $scope.variants['12881']['shipping_height'] = "50";
 
-  $scope.user    = { "name":"", "email":"", "password":"", "password_confirmation":""};
-  $scope.farmer  = {"favorite_quotes":"", "role_models":"", "personal_philosophy":""};
+  $scope.products = {};
+
+  $scope.board_products = {};
+
+  $scope.init = function(){
+
+    //initialize hashes with ids as keys for lookup when trying to update all ajax style
+
+  }
 
   $scope.deleteProductFromBoard = function(){
 
   }
 
-  $scope.submitForms = function(){
-    submitData("user");
-  }
-
-  submitData = function(model, id){
-    var modelToSubmit = {"ajax":true};
-    modelToSubmit[model] = $scope[model]
-    var path = '/' + model + 's';
-    if(model === "farmer"){
-      modelToSubmit["user_id"] = id;
-    }
-
-
-    $http.post(path, modelToSubmit).
-    success( function(response){
-      if( response.id ){
-        if(model === "user"){
-          submitData("farmer", response.id)
-        }
-        else{
-          window.location.href = "/farms/new";
-        }
-      }
-      else{
-        $scope.user_errors = JSON.stringify(response)
-      }
-    }).
-    error( function(response){
-      alert('bad mkay'); 
-    }); 
+  $scope.approve_product_for_board = function(product_id, board_id){
+    alert( "Approving product " + product_id + " on board " + board_id);
   }
 
 }
