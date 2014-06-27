e9f9fbff123768f8cda1cd378e3a3b051cc21878
Luke Parham <lappp9@gmail.com>
2014-06-26 23:03:22 -0500

    may have made the boards page a little better

--- app/controllers/spree/admin/boards_controller.rb
+++ app/controllers/spree/admin/boards_controller.rb
@@ -1,28 +1,18 @@
 class Spree::Admin::BoardsController < Spree::Admin::ResourceController
 
-
- 
   def index
+    @boards         = Spree::Board.includes(:board_products).page(params[:page]).per(params[:per_page] || 10)
 
-    #@boards = @boards.select do |board| 
-      #board_product_count = 0
-      #board.board_products.each do |bp|
-        #board_product_count += 1 unless bp.product.nil?
-      #end
+    board_products = @boards.each.map(&:board_products).flatten(1)
+    
+    products       = board_products.map(&:product).compact
 
-      #puts "#{board.name} has #{board_product_count} products"
-      #board_product_count > 0
-    #end
-    @boards = Spree::Board.all.page(params[:page]).
-      per(params[:per_page] || 10)
-    @board_products = Spree::BoardProduct.where( approved_at: nil, removed_at: nil)
-    #@board_products = Spree::BoardProduct.all.select {|bp| bp.approved_at == nil && bp.removed_at == nil  }
-    @products       = @board_products.map(&:product).compact
-    @suppliers      = @products.map(&:supplier).compact.uniq
-    designers = @boards.collect(&:designer).collect { |d| "#{d.first_name} #{d.last_name}"}.uniq
+    @suppliers     = products.map(&:supplier).compact.uniq
+
+    designers       = @boards.collect(&:designer).collect { |d| "#{d.first_name} #{d.last_name}"}.uniq
+
     @designer_names = ["All designers"] + designers
   end
-
 
   def new
     @board = Spree::Board.new(:name => "Untitled Room")
@@ -30,15 +20,4 @@ class Spree::Admin::BoardsController < Spree::Admin::ResourceController
     @board.save!
   end
 
-  
-  # redirect to the edit action after create
-  #  create.response do |wants|
-  #    wants.html { redirect_to edit_admin_fancy_thing_url( @fancy_thing ) }
-  #  end
-  #  
-  #  # redirect to the edit action after update
-  #  update.response do |wants|
-  #    wants.html { redirect_to edit_admin_fancy_thing_url( @fancy_thing ) }
-  #  end
- 
 end
