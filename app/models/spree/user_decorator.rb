e36a0d0fdd59bf32c5fe632bba13b7c9e1ec4f8b
szax <szax22@gmail.com>
2015-05-28 16:00:06 +0200

    - fix imge user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -8,7 +8,7 @@ Spree::User.class_eval do
   has_many :marketing_images, as: :viewable, dependent: :destroy, class_name: "Spree::MarketingImage"
   has_one :logo_image, as: :viewable, dependent: :destroy, class_name: "Spree::LogoImage"
   has_one :feature_image, as: :viewable, dependent: :destroy, class_name: "Spree::FeatureImage"
-  #accepts_nested_attributes_for :user_images, :logo_image, :marketing_images, :feature_image
+  accepts_nested_attributes_for :user_images, :logo_image, :marketing_images, :feature_image
   is_impressionable
   
   def self.designers
