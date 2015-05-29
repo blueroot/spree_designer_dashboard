e44c2c5e2491bc7c208b88eeeee08f41eeb9607c
szax <szax22@gmail.com>
2015-05-29 20:22:44 +0200

    - avatar

--- app/controllers/spree/designers_controller.rb
+++ app/controllers/spree/designers_controller.rb
@@ -1,8 +1,8 @@
 class Spree::DesignersController < Spree::StoreController
   before_filter :require_authentication, :only => [:update]
-  before_filter :set_section 
-  
-  impressionist :actions=>[:show]
+  before_filter :set_section
+
+  impressionist :actions => [:show]
 
   def set_section
     @selected_section = "designers"
@@ -11,38 +11,51 @@ class Spree::DesignersController < Spree::StoreController
   def index
     @designers = Spree::User.published_designers().order("created_at desc")
   end
-  
+
   def tutorials
-    
+
   end
-  
+
   def update
+    if params[:user].present? and params[:user][:user_images_attributes].present? and params[:user][:user_images_attributes]['0'].present?
+      data = StringIO.new(Base64.decode64((params[:user][:user_images_attributes]['0'][:attachment])))
+      data.class.class_eval { attr_accessor :original_filename, :content_type }
+      data.original_filename = "avatar.png"
+      data.content_type = "image/png"
+      params[:user][:user_images_attributes]['0'][:attachment] = data
+    end
+
     @user = spree_current_user
     respond_to do |format|
-      if @user.update_attributes(designer_params)
+      if @user.update_attributes(params[:user].permit!)
         format.html { redirect_to my_profile_path, :notice => 'Your profile was successfully updated.' }
       else
         format.html { redirect_to my_profile_path, :notice => 'There was an error and your profile was not updated.' }
       end
-    end    
+    end
   end
-  
+
   def signup
     @designer = Spree::User.new
   end
-  
+
   def show
     @designer = Spree::User.is_active_designer().where(:username => params[:username]).first
     @products = []
     @products = @designer.products.available_through_published_boards if @designer.present?
     render :action => "show"
   end
-  
-  private 
-    def designer_params
-      params.require(:user).permit(:first_name, :last_name, :description, :company_name, :website_url, :location, :blog_url, :email, :password, :password_confirmation, :is_discount_eligible, :is_beta_user, :can_add_boards, :designer_featured_starts_at, :designer_featured_ends_at, :designer_featured_position, :supplier_id, :marketing_images_attributes, :user_images_attributes, :logo_image_attributes, :feature_image_attributes,  :bill_address_id, :ship_address_id, :social_facebook, :social_twitter, :social_instagram, :social_pinterest, :social_googleplus, :social_linkedin, :social_tumblr, :username, :designer_quote, :marketing_images, :profile_display_name, :designer_commission, :show_designer_profile, :feature_image)
-    end
-   
+
+  private
+  def designer_params
+    params.require(:user).permit(:first_name, :last_name, :description, :company_name, :website_url, :location, :blog_url, :email, :password, :password_confirmation,
+                                 :is_discount_eligible, :is_beta_user, :can_add_boards, :designer_featured_starts_at, :designer_featured_ends_at, :designer_featured_position,
+                                 :supplier_id, :marketing_images_attributes, :logo_image_attributes, :feature_image_attributes, :bill_address_id, :ship_address_id,
+                                 :social_facebook, :social_twitter, :social_instagram, :social_pinterest, :social_googleplus, :social_linkedin, :social_tumblr,
+                                 :username, :designer_quote, :marketing_images, :profile_display_name, :designer_commission, :show_designer_profile, :feature_image,
+                                 :user_images_attributes)
+  end
+
 end
 
 
