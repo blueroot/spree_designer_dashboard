1e0ea5fd9fa9d2dab7ce1e8775ffa59e038369f3
szax <szax22@gmail.com>
2015-06-24 15:29:12 +0200

    - fixes filters

--- app/models/spree/user_image.rb
+++ app/models/spree/user_image.rb
@@ -8,7 +8,7 @@ module Spree
                       default_style: :primary,
                       url: '/spree/user_images/:id/:style/:basename.:extension',
                       path: 'user_images/:id/:style/:basename.:extension',
-                      convert_options: { all: '-strip -auto-orient -colorspace RGB' }
+                      convert_options: { all: '-strip -auto-orient' }
 
     validates_attachment_presence :attachment
     validate :no_attachment_errors
