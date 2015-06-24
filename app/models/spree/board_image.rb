9cc50556434085445c8716bf30c9143bbb9b127e
szax <szax22@gmail.com>
2015-06-24 14:53:54 +0200

    - fix image in rooms

--- app/models/spree/board_image.rb
+++ app/models/spree/board_image.rb
@@ -9,7 +9,7 @@ module Spree
                       default_style: :primary,
                       url: '/spree/board_image/:id/:style/:basename.:extension',
                       path: 'board_image/:id/:style/:basename.:extension',
-                      convert_options: { all: '-strip -auto-orient -colorspace RGB' }
+                      convert_options: { all: '-strip -auto-orient' }
     validates_attachment_presence :attachment
     validate :no_attachment_errors
 
