52eb0705275a9c14487f363b5fd2ecdf41212bdb
zajac <zajac@ubuntu-14.04-default>
2015-11-05 17:33:51 +0100

    - change arrival product numbers in home page

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -5,7 +5,7 @@ module Spree
     def home2
       @slides = Spree::Slide.current.order("created_at desc") || Spree::Slide.defaults
       @page_photos = Spree::PagePhoto.where(active: true)
-      @arrive_product = Spree::Product.where(new_arrival: true).where("new_arrival_until >= ?", DateTime.now.to_date)
+      @arrive_product = Spree::Product.where(new_arrival: true).where("new_arrival_until >= ?", DateTime.now.to_date).limit(20)
       @designers = Spree::User.published_designers().order("created_at desc")
       @promoted_rooms = Spree::Board.promoted.limit(6)
       @home_text = Spree::HomeText.first
